7a2c8839bbd2fdaa79071c0970c43ab17da6798c
Siddhant Mulay <mulay.siddhant@gmail.com>
2020-04-17 16:21:50 +0530

    Section creation WIP

--- src/components/PageComponents/Landing/Section/Section.js
+++ src/components/PageComponents/Landing/Section/Section.js
@@ -1,12 +1,22 @@
+
 import React, { Component } from 'react';
 import './Section.css';
+import productImg from '../../../../assets/images/Product.png';
 
 class Section extends Component {
 
     render() {
         return (
-            <div>
-                Yo!
+            <div className="section">
+                <div className="row">
+                    <div className="col-lg-6 col-xl-6 imgContainer">
+                        <img src={productImg} alt="" />
+                    </div>
+                    <div className="col-lg-6 col-xl-6 dataContainer">
+                        Test
+                    </div>
+                </div>
+
             </div>
         )
     }
