5f654ef03f95caec94c73b83e3683fc8ff10dbe9
Siddhant Mulay <mulay.siddhant@gmail.com>
2020-04-18 11:06:09 +0530

    Product Card partly optimized

---
+++ src/components/PageComponents/Landing/ProductCard/ProductCard.js
@@ -0,0 +1,118 @@
+
+import React, { Component } from 'react';
+import './ProductCard.css';
+import LineIcon from 'react-lineicons';
+
+class ProductCard extends Component {
+
+    radioCheck = () => { }
+
+    renderNotes = () => {
+        const { data } = this.props;
+        const notes = data.notes;
+        let retArr = [];
+        if (notes.length > 0) {
+            notes.map((item, index) => {
+                retArr.push(<li key={`lol${index}`}>
+                    <span className="title">{item.title}:</span>
+                    <span className="desc">{item.desc}</span>
+                </li>)
+            });
+        }
+        return retArr;
+    }
+
+    renderColorSelection = () => {
+        const { data } = this.props;
+        const colors = data.colors;
+        let retArr = [];
+        if (colors.length > 0) {
+            colors.forEach((item, index) => {
+                retArr.push(<div key={`color${index}`} className={`colorSelect ${item}`}>
+                    <input type="radio" name="colorsel" id="" onChange={this.radioCheck} />
+                    <div className={`checkMark ${item}`}></div>
+                </div>)
+            });
+        }
+        return retArr;
+    }
+
+    render() {
+        const { data } = this.props;
+        return (
+            <div className="productCard">
+                <div className="row">
+                    <div className="col-md-6 col-lg-6 col-xl-6 imgContainer">
+                        <img src={data.img} alt="" />
+                    </div>
+                    <div className="col-md-6 col-lg-6 col-xl-6 dataContainer">
+                        <div className="productDetails">
+                            <div className="name text">
+                                {data.name}
+                            </div>
+
+                            <div className="price">
+                                {data.oldPrice ?
+                                    <div className="oldPrice">
+                                        {data.oldPrice}
+                                    </div>
+                                    : null}
+                                <div className="newPrice">
+                                    {data.newPrice}
+                                </div>
+                            </div>
+
+                            <div className="notes">
+                                <ul>
+                                    {this.renderNotes()}
+                                </ul>
+                            </div>
+
+                            <div className="attributes">
+                                <div className="feature">
+                                    <div className="icon">
+                                        <LineIcon name="car" />
+                                    </div>
+                                    <div className="title">
+                                        SAME DAY DISPATCH
+                                    </div>
+                                </div>
+
+                                <div className="feature">
+                                    <div className="icon">
+                                        <LineIcon name="reload" />
+                                    </div>
+                                    <div className="title">
+                                        EASY - 30 DAY RETURN POLICY
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="color">
+                                <div className="title">Color:</div>
+
+                                <div className="colorSelection">
+                                    {this.renderColorSelection()}
+                                </div>
+                            </div>
+
+                            <div className="cardActions">
+                                <div className="counter">
+                                    <LineIcon name="minus" />
+                                    <span className='count'>1</span>
+                                    <LineIcon name="plus" />
+                                </div>
+                                <button className="custButton">
+                                    Add to Cart
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default ProductCard;
--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import AllBreadcrumbs from '../../components/Common/Breadcrumbs/AllBreadcrumbs';
 import Sidebar from '../../components/Common/Sidebar/Sidebar';
 import './Landing.css';
+import ProductCard from '../../components/PageComponents/Landing/ProductCard/ProductCard';
+import productImg from '../../assets/images/Product.png';
 import Section from '../../components/PageComponents/Landing/Section/Section';
 
 class Landing extends Component {
@@ -20,13 +22,32 @@ class Landing extends Component {
             current: true
         }]
 
+        const productData = {
+            img: productImg,
+            name: "Nudie Extendable Sofa for 3 persons.",
+            oldPrice: "$169",
+            newPrice: "$149",
+            notes: [{
+                title: 'Assembly',
+                desc: 'Carpenter Assembly'
+            }, {
+                title: 'Warranty',
+                desc: "12 Months' Warranty"
+            }],
+            colors: [
+                'beige', 'black', 'white'
+            ]
+        }
+
         return (
             <div>
                 <AllBreadcrumbs data={breadcrumbData} />
                 <div className="landingContent">
                     <Sidebar />
                     <div className="allSections">
-                        <Section />
+                        <ProductCard
+                            data={productData}
+                        />
                     </div>
                 </div>
             </div>
